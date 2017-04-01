4b4e5cfbbfa6f881a9c23bed35caac3bd54adca2
John Hill <nouveau.pg@gmail.com>
2017-03-31 23:54:45 -0400

    Initial Commit

---
+++ TonalTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TonalTest
+//
+//  Created by John Hill on 3/31/17.
+//  Copyright © 2017 John Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
